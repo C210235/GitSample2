71a6383b4c87ec8d7eae8580b92b716fb307ac32
C210235 <91927060+C210235@users.noreply.github.com>
2021-10-25 08:39:20 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("heloo world");
+        System.out.println(args.length);
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+    }
+}
